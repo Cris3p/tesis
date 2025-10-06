14c10bd306023ab79f10ee6568a9cb97a4c4a9fb
Cris3p <112230149+Cris3p@users.noreply.github.com>
2025-10-06 02:49:38 -0300

    Delete server directory

--- server/routes/contactosRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const contactosController = require("../controllers/contactosController");
-
-router.post("/", contactosController.agregarContacto);
-router.get("/:id_usuario", contactosController.getContactosByUsuario);
-router.delete("/:id", contactosController.eliminarContacto);
-
-module.exports = router;
