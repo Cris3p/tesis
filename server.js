f3b129fe7c9ca744ba6d9b32d7d8218b249107c8
Valenn-art <valentinamh35@gmail.com>
2025-10-20 15:01:57 -0300

    testdb

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const app = require ('./server/app');
 const port = process.env.PORT || 4000;
 
+app.get("/testdb", async (req, res) => {
+  try {
+    const [rows] = await pool.query("SELECT NOW() AS hora");
+    res.json({ conexion: "ok", hora: rows[0].hora });
+  } catch (err) {
+    console.error("Error de conexión:", err);
+    res.status(500).json({ error: "Error conectando a la base de datos" });
+  }
+});
+
 app.listen(port, () => {
   console.log(`Servidor corriendo en http://localhost:${port}`);
 });
